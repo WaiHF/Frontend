8d159bb25d97cbe8a0710347efd1172a138cd1a5
Wai <waihfun@gmail.com>
2022-12-14 23:49:10 +0000

    Implemented Azure function call

---
+++ visitcount.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+    const url = window.location.href
+    const functionURL = 'https://whf-functions.azurewebsites.net/api/orc/VisitIncrement?page='
+
+    fetch(url)
+        .then((response) => {
+            // Splits the URL by / and gets the last item in the array which will be the page name
+            let pageName = url.split("/").pop();
+
+            // 404 and index might not show in the URL so they need to be processed in the statement below
+            if (response.status == '404') {
+                pageName = '404'
+            } else if (pageName == '') {
+                pageName = 'index'
+            } else {
+                // Splits the page name by the . and gets the first item which will be the page name without the extension
+                pageName = pageName.split('.')[0]
+            }
+
+            // Calls the StartVisitIncrement Azure function.
+            fetch(functionURL + pageName, {
+                method: 'PUT'
+            })
+                // Returns the function's response by running .json() on recieved data.
+                .then(funcResponse => funcResponse.json())
+                .then((funcData) => {
+                    // After 2 seconds try to to visit function's result page.
+                    // Maybe can change this to a loop instead of waiting for 2 seconds.
+                    setTimeout (() => fetch(funcData.statusQueryGetUri)
+                    .then(funcResult => funcResult.json())
+                    .then(result => console.log('There have been '  + result.output.visits + ' to this page.'))
+                    .catch(error => console.log('Failed to get function result: ' + error))
+                    , 2000)
+                    
+                })
+                .catch(error => console.log('Failed to run function: ' + error));
+        })
+        .catch(error => console.error('Failed to check status: ' + error));
+}
